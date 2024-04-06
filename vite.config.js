313e99a45765780e8c0d394190964fb6d4a57ecd
Ersin Akinci <me@ersinakinci.com>
2024-04-06 13:59:43 -0700

    Comment Vite config

--- vite.config.js
+++ vite.config.js
@@ -13,12 +13,17 @@ export default defineConfig(({ command }) => {
         },
         external: ["node:zlib", "bun:sqlite"],
       },
+      // We're not actually using SSR, but using the ssr option compiles
+      // the server and enables the ssrEmitAssets option.
       ssr: "./src/index.tsx",
+      // Emit global CSS and client JS pulled in by '...?url' imports.
       ssrEmitAssets: true,
+      // Generate a manifest.json file that can be used to map compiled CSS modules to components.
       manifest: true,
       target: "es2022",
     },
     plugins: [
+      // Experimental plugin to emit CSS modules as chunks.
       command === "build" && emitCssModules(),
       devServer({
         entry: "./src/index.tsx",
